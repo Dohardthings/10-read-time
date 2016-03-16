648be07ea0c08accc3e27c8b289a236583a4a4e8
Daniel McBryde <mcbryded@gmail.com>
2016-03-16 18:16:39 -0500

    app part 1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 'use strict';
 /* globals numWords, readTime */
+
+const info = document.querySelectorAll(`.entry__info`);
+const contents = document.querySelectorAll(`.entry__content`);
+
+for (let i = 0; i < info.length; i++) {
+  const timeEl = document.createElement(`li`);
+  timeEl.classList.add(`read-time`);
+  const loop = info[i];
+  const blogPost = contents[i];
+
+  loop.appendChild(timeEl);
+
+  timeEl.innerText = `${readTime(blogPost.innerText, 200)} mins`;
+}
