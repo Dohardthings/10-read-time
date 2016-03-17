57a53c34973f1ed7a262964d41262d4da87f42e9
Daniel McBryde <mcbryded@gmail.com>
2016-03-16 20:02:09 -0500

    last testpassed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 'use strict';
 /* globals numWords, readTime */
-
+const entries = document.querySelectorAll(`.entry`);
+const bodyEl = document.querySelector(`body`);
+const nav = document.createElement(`nav`);
+bodyEl.appendChild(nav);
 const info = document.querySelectorAll(`.entry__info`);
-const contents = document.querySelectorAll(`.entry__content`);
+const posts = document.querySelectorAll(`.entry__content`);
+const postTitles = document.querySelectorAll(`.entry__title`);
 
 for (let i = 0; i < info.length; i++) {
   const timeEl = document.createElement(`li`);
+  const link = document.createElement(`a`);
+  const currentInfo = info[i];
+  const currentPost = posts[i];
+
   timeEl.classList.add(`read-time`);
-  const loop = info[i];
-  const blogPost = contents[i];
+  currentInfo.appendChild(timeEl);
+  nav.appendChild(link);
+  link.setAttribute(`href`, `#${entries[i].id}`);
+  link.innerText = `${postTitles[i].innerText}`;
 
-  loop.appendChild(timeEl);
-
-  timeEl.innerText = `${readTime(blogPost.innerText, 200)} mins`;
+  timeEl.innerText = `${readTime(currentPost.innerText, 200)} mins`;
 }
